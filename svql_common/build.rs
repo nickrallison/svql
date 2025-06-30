6399e9a90de2d7a55db4c27608f55a63684df07b
Nick Allison <99363282+nickrallison@users.noreply.github.com>
2025-06-30 14:09:22 -0600

    Fixed cargo issues

--- svql_common/build.rs
+++ svql_common/build.rs
@@ -6,21 +6,21 @@ use std::{
 
 fn main() {
     // rebuild triggers ------------------------------------------------------
-    println!("cargo:rerun-if-changed=src/");
-    println!("cargo:rerun-if-env-changed=CARGO_MANIFEST_DIR");
+    // println!("cargo:rerun-if-changed=src/");
+    // println!("cargo:rerun-if-env-changed=CARGO_MANIFEST_DIR");
 
-    let crate_dir =
-        PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set"));
+    // let crate_dir =
+    //     PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set"));
 
-    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not set"));
-    let include_dir = out_dir.join("include");
-    let header_path = include_dir.join("source.h");
+    // let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not set"));
+    // let include_dir = out_dir.join("include");
+    // let header_path = include_dir.join("source.h");
 
-    Builder::new()
-        .with_crate(&crate_dir)
-        .with_language(Language::C)
-        .with_include_guard("SVQL_COMMON_SOURCE_H")
-        .generate()
-        .expect("cbindgen failed")
-        .write_to_file(&header_path);
+    // Builder::new()
+    //     .with_crate(&crate_dir)
+    //     .with_language(Language::C)
+    //     .with_include_guard("SVQL_COMMON_SOURCE_H")
+    //     .generate()
+    //     .expect("cbindgen failed")
+    //     .write_to_file(&header_path);
 }
