b6535a58f04ffbcc1b04b2b35c47f9c588b6c9ce
Nick Allison <99363282+nickrallison@users.noreply.github.com>
2025-07-03 00:08:49 -0600

    yosys progress

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,3 +7,5 @@ cp scripts/yosys_BUILD_bazel yosys/BUILD.bazel
 # rm yosys/libs/cxxopts/WORKSPACE
 # Remove vendored cxxopts bazel file, should be found by glob
 rm -f yosys/libs/cxxopts/BUILD.bazel yosys/libs/cxxopts/WORKSPACE
+find yosys/ -mindepth 2 -name "BUILD.bazel" -delete
+find yosys/ -mindepth 2 -name "BUILD" -delete
