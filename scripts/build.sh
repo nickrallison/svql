310049dd1489ffe358d1bc635df6a1f86402bc75
Nick Allison <99363282+nickrallison@users.noreply.github.com>
2025-07-03 14:34:48 -0600

    Cleaning up / cmake

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bazel build --jobs 32 --cpu=multi-threaded //yosys:yosys --show_progress --worker_verbose --verbose_failures --sandbox_debug
-# bazel build --jobs 32 --cpu=multi-threaded //yosys:cxxopts_header --show_progress --worker_verbose --verbose_failures --sandbox_debug
+cmake -B build; 
+cmake --build build --parallel 32
