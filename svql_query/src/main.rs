f34404a7485389ff9730a954e78036a11f353bbf
nickrallison <nickrallison1@gmail.com>
2025-07-30 17:46:48 -0600

    Working on structure

--- svql_query/src/main.rs
+++ svql_query/src/main.rs
@@ -1,85 +1,7 @@
-// #![allow(unused_imports)]
-// use crate::driver::mock::MockDriver;
-// use crate::driver::Driver;
-// use crate::examples::and::And;
-// use crate::examples::double_and::DoubleAnd;
-// use crate::examples::triple_and::TripleAnd;
-// use crate::module::RtlModule;
-// use crate::query::traits::RtlQueryTrait;
-// use crate::query::RtlQuery;
-// use log::LevelFilter;
-// use svql_common::matches::SanitizedQueryMatch;
-
-// mod driver;
-// mod examples;
-// mod module;
-// mod port_spec;
-mod instance;
-
-// fn main() {
-//     // Initialize logger
-//     env_logger::Builder::new()
-//         .filter_level(LevelFilter::Trace)
-//         .init();
-
-//     log::trace!("Starting svql_query");
-
-//     // Directly query the driver for AND gate matches
-//     // let triple_and: RtlQuery<TripleAnd> = RtlQuery::new(TripleAnd::new(), "triple_and".to_string());
-//     let and = RtlModule::<And>::root("and_gate".to_string());
-    
-//     // let driver = Driver::new_net("localhost:9999".to_string());
-//     let driver = Driver::new_mock();
-
-//     match and.query(&driver) {
-//         Ok(matches) => {
-//             log::trace!("And query returned {} matches", matches.len());
-//             for m in matches.iter() {
-//                 println!("-----\n{:#?}", m);
-//             }
-//         }
-//         Err(e) => {
-//             log::trace!("Triple And query failed with error: {:?}", e);
-//         }
-//     }
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn test_and_query() {
-//         let and = RtlModule::<And>::new(And::new(), "and_gate".to_string());
-//         let driver = Driver::new_mock();
-//         let matches = and.query(&driver);
-//         assert!(matches.is_ok());
-//         let matches = matches.unwrap();
-//         for m in matches.iter() {
-//             println!("-----\n{:#?}", m);
-//         }
-//     }
-// }
-
 use crate::instance::Instance;
-// use crate::module::lookup;
-// use crate::module::traits::{RtlModuleResultTrait, RtlModuleTrait};
-// use crate::ports::{InPort, OutPort};
-// use std::collections::{HashMap, VecDeque};
-// use std::path::PathBuf;
-// use std::sync::Arc;
-// use svql_common::id_string::IdString;
-// use svql_query_proc_macro::module;
+use std::collections::HashSet;
 
-// #[module(
-//     file = "examples/patterns/basic/and/verilog/and.v",
-//     module = "and_gate",
-//     yosys = "./yosys/yosys",
-//     svql_pat_plugin_path = "./build/svql_pat_lib/libsvql_pat_lib.so"
-// )]
-// pub struct And;
-
-
+mod instance;
 
 // ########################
 // Base Search Types
@@ -91,6 +13,34 @@ pub struct Search;
 pub struct Match;
 
 // ########################
+// Driver
+// ########################
+
+pub struct Driver;
+
+// ########################
+// Traits
+// ########################
+
+pub trait Searchable: Clone + Default {
+    type Hit;
+    fn query(driver: &Driver, path: Instance) -> Vec<Self::Hit>;
+    fn permutations(&self) -> Vec<Self>;
+}
+
+pub trait Netlist {
+    fn module_name() -> &'static str;
+    fn file_path() -> &'static str;
+    fn yosys() -> &'static str;
+    fn svql_driver_plugin() -> &'static str;
+
+    // ####
+    fn swappable() -> Vec<HashSet<String>>;
+}
+
+pub trait Composite {}
+
+// ########################
 // Containers
 // ########################
 
@@ -100,7 +50,7 @@ pub struct Wire<T> {
     pub val: T,
 }
 
-impl <T> Wire<T> where T: Default {
+impl <T: Default> Wire<T> {
     pub fn root(name: String) -> Self {
         let path = Instance::root(name);
         Self::new(path)
@@ -131,9 +81,28 @@ impl<T: Default> And<T> {
     }
 }
 
-impl And<Search> {
-    pub fn query(&self) -> Vec<And<Match>> {
-       todo!();
+impl Searchable for And<Search> {
+    type Hit = And<Match>;
+    fn query(driver: &Driver, path: Instance) -> Vec<Self::Hit> {
+        todo!("This should be a simple call to the driver's query method");
+    }
+}
+
+impl<T> Netlist for And<T> {
+    fn module_name() -> &'static str {
+        "and_gate"
+    }
+    fn file_path() -> &'static str {
+        "./examples/patterns/basic/and/verilog/and.v"
+    }
+    fn yosys() -> &'static str {
+        "./yosys/yosys"
+    }
+    fn svql_driver_plugin() -> &'static str {
+        "./build/svql_driver/libsvql_driver.so"
+    }
+    fn swappable() -> Vec<HashSet<String>> {
+        vec![HashSet::from(["a".to_string(), "b".to_string()])]
     }
 }
 
@@ -156,6 +125,15 @@ impl<T: Default> DoubleAnd<T> {
     }
 }
 
+impl Searchable for DoubleAnd<Search> {
+    type Hit = DoubleAnd<Match>;
+    fn query(driver: &Driver, path: Instance) -> Vec<Self::Hit> {
+        todo!("This should be calls to query for each And in DoubleAnd, compose them with itertools' cartesian product, then a filter to combine them");
+    }
+}
+
+impl<T> Composite for DoubleAnd<T> {}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TripleAnd<T> {
     pub path: Instance,
@@ -175,10 +153,50 @@ impl<T: Default> TripleAnd<T> {
     }
 }
 
+impl Searchable for TripleAnd<Search> {
+    type Hit = TripleAnd<Match>;
+    fn query(driver: &Driver, path: Instance) -> Vec<Self::Hit> {
+        todo!("This should look similar to DoubleAnd's query, but with a call to double_and, and then a call to and");
+    }
+}
+
+pub enum RecursiveAnd<T> {
+    BaseCase(And<T>),
+    RecursiveCase(Box<RecursiveAnd<T>>),
+}
+
+impl<T> RecursiveAnd<T> {
+    pub fn root_base(name: String) -> Self {
+        let path = Instance::root(name);
+        Self::new_base(path, base)
+    }
+    pub fn new_base(path: Instance) -> Self {
+        let and = And::new(path.child("and".to_string()));
+        Self::BaseCase(and)
+    }
+
+    // ###
+
+    pub fn recursive(val: RecursiveAnd<T>) -> Self {
+        Self::RecursiveCase(Box::new(val))
+    }
+}
+
+impl Searchable for RecursiveAnd<Search> {
+    type Hit = RecursiveAnd<Match>;
+    fn query(driver: &Driver, path: Instance) -> Vec<Self::Hit> {
+        todo!("TODO: Work upwards from the base case and filtering each step until no more is found");
+    }
+}
+
 fn main() {
     let and_search: And<Search> = And::root("and".to_string());
     let double_and_search: DoubleAnd<Search> = DoubleAnd::root("double_and".to_string());
     let triple_and_search: TripleAnd<Search> = TripleAnd::root("triple_and".to_string());
+
+
+
+    // can't define enum, don't need to specify which type, just want all of them
 
     // let and_matches: Vec<And<Match>> = and_search.query().unwrap();
     // let double_and_matches: Vec<DoubleAnd<Match>> = double_and_search.query().unwrap();
