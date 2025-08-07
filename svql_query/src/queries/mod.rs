7c1a5c3bfd8c24d2dcb077ed435ad4cff9946d00
nick <nickrallison1@gmail.com>
2025-08-06 22:37:32 -0600

    more examples

--- svql_query/src/queries/mod.rs
+++ svql_query/src/queries/mod.rs
@@ -1,5 +1,15 @@
+use std::ops::Not;
+
+use itertools::iproduct;
+
+use crate::composite::{Composite, SearchableComposite};
+use crate::driver::Driver;
 use crate::instance::Instance;
-use crate::State;
+use crate::{Connection, Match, Search, State, WithPath};
+use crate::composite::{MatchedComposite};
+use crate::netlist::SearchableNetlist;
+use crate::queries::basic::and::And;
+use crate::{Wire};
 
 pub mod basic;
 pub mod examples;
@@ -14,6 +24,48 @@ where
     pub not_lock_or_dbg_and_write: NotLockOrDebugAndWrite<S>,
 }
 
+impl<S> WithPath<S> for Cwe1234<S>
+where
+    S: State,
+{
+    fn new(path: Instance) -> Self {
+        todo!()
+    }
+
+    crate::impl_find_port!(Cwe1234, locked_reg, not_lock_or_dbg_and_write);
+    fn path(&self) -> Instance {
+        self.path.clone()
+    }
+}
+
+impl<S> Composite<S> for Cwe1234<S>
+where
+    S: State,
+{
+    fn connections(&self) -> Vec<Vec<Connection<S>>> {
+        todo!()
+    }
+}
+
+impl SearchableComposite for Cwe1234<Search> {
+    type Hit = Cwe1234<Match>;
+    fn query(driver: &Driver, path: Instance) -> Vec<Self::Hit> {
+        let locked_reg_search_result: Vec<LockedReg<Match>> =
+            LockedReg::<Search>::query(driver, path.child("locked_reg".to_string()));
+        let not_lock_or_dbg_and_write_search_result: Vec<NotLockOrDebugAndWrite<Match>> =
+            NotLockOrDebugAndWrite::<Search>::query(driver, path.child("not_lock_or_dbg_and_write".to_string()));
+        let results = iproduct!(locked_reg_search_result, not_lock_or_dbg_and_write_search_result)
+            .map(|(locked_reg, not_lock_or_dbg_and_write)| Self::Hit {
+                locked_reg,
+                not_lock_or_dbg_and_write,
+                path: path.clone(),
+            })
+            .filter(|s| Self::Hit::validate_connections(s, s.connections()))
+            .collect::<Vec<_>>();
+        results
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum LockedReg<S>
 where
@@ -23,6 +75,12 @@ where
     MuxAsync(LockedRegMuxAsync<S>),
     EnSync(LockedRegEnSync<S>),
     EnAsync(LockedRegEnAsync<S>),
+}
+
+impl LockedReg<Search> {
+    pub fn query(driver: &Driver, path: Instance) -> Vec<LockedReg<Match>> {
+        todo!()
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -45,4 +103,10 @@ pub struct LockedRegEnAsync<S> {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct NotLockOrDebugAndWrite<S> {
     val: S,
+}
+
+impl NotLockOrDebugAndWrite<Search> {
+    pub fn query(driver: &Driver, path: Instance) -> Vec<NotLockOrDebugAndWrite<Match>> {
+        todo!()
+    }
 }
