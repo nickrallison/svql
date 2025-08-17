ce091f5065fd20b9e2618c5b374d8627113888ad
nick <nickrallison1@gmail.com>
2025-08-17 13:49:59 -0600

    working on integration tests

--- svql_driver/tests/integration_tests.rs
+++ svql_driver/tests/integration_tests.rs
@@ -2,25 +2,22 @@
 #[cfg(test)]
 mod integration_tests {
     use std::path::PathBuf;
+    use svql_driver::Driver;
 
-    use prjunnamed_netlist::Design;
-    use svql_driver::{driver::DesignPath, read_input_to_design, Driver};
-
-    use super::*;
 
     lazy_static::lazy_static! {
+
         static ref COMB_D_DOUBLE_SDFFE: Driver = load_driver_from("examples/patterns/basic/ff/comb_d_double_sdffe.v");
+        static ref AND_Q_DOUBLE_SDFFE: Driver = load_driver_from("examples/patterns/basic/ff/and_q_double_sdffe.v");
         static ref SEP_DOUBLE_SDFFE: Driver = load_driver_from("examples/patterns/basic/ff/sep_double_sdffe.v");
+        static ref SDFFE: Driver = load_driver_from("examples/patterns/basic/ff/sdffe.v");
     }
 
     fn load_driver_from(path: &str) -> Driver {
         let path = std::path::PathBuf::from(path);
         let file_stem = PathBuf::from(path.file_stem().expect("Failed to get file stem"));
         let name = file_stem.file_name().expect("Failed to get file name").to_str().expect("Failed to convert file name to str").to_string();
-
-        let driver = Driver::new(path, name, None)
-            .expect("Failed to create driver");
-        driver
+        Driver::new(path, name, None).expect("Failed to create driver")
     }
 
     #[test]
@@ -35,4 +32,43 @@ mod integration_tests {
         assert_eq!(sep_search_matches.len(), 2, "Expected 2 matches for needle sep, against haystack comb, got {}", sep_search_matches.len());
     }
 
+}
+
+#[cfg(test)]
+mod integration_tests_2 {
+    use std::path::{Path, PathBuf};
+
+    use super::*;
+    use rstest::rstest;
+    use svql_driver::Driver;
+
+    lazy_static::lazy_static! {
+
+        static ref COMB_D_DOUBLE_SDFFE: (Driver, PathBuf) = load_driver_from("examples/patterns/basic/ff/comb_d_double_sdffe.v");
+        static ref AND_Q_DOUBLE_SDFFE: (Driver, PathBuf) = load_driver_from("examples/patterns/basic/ff/and_q_double_sdffe.v");
+        static ref SEP_DOUBLE_SDFFE: (Driver, PathBuf) = load_driver_from("examples/patterns/basic/ff/sep_double_sdffe.v");
+        static ref SDFFE: (Driver, PathBuf) = load_driver_from("examples/patterns/basic/ff/sdffe.v");
+    }
+
+    fn load_driver_from(path: &str) -> (Driver, PathBuf) {
+        let path = std::path::PathBuf::from(path);
+        let file_stem = PathBuf::from(path.file_stem().expect("Failed to get file stem"));
+        let name = file_stem.file_name().expect("Failed to get file name").to_str().expect("Failed to convert file name to str").to_string();
+        let driver = Driver::new(path, name, None).expect("Failed to create driver");
+        (driver, file_stem)
+    }
+
+   
+    #[rstest]
+    #[case(&COMB_D_DOUBLE_SDFFE, &COMB_D_DOUBLE_SDFFE, &SEP_DOUBLE_SDFFE.0, 0)]
+    fn test_subgraph_matches(
+        #[case] needle_tuple: &'static (Driver, PathBuf),
+        #[case] haystack_tuple: &'static (Driver, PathBuf),
+        #[case] expected: usize,
+    ) {
+        let (needle, needle_path) = needle_tuple;
+        let (haystack, haystack_path) = haystack_tuple;
+        let matches = svql_driver::subgraph::find_subgraphs(needle.design_as_ref(), haystack.design_as_ref());
+        assert_eq!(matches.len(), expected, "Expected {} matches for needle {}, against haystack {}, got {}", expected, needle_path.display(), haystack_path.display(), matches.len());
+    }
 }
