78470d8935762aff88c1f6ce8e5cedc33dd4e937
nick <nickrallison1@gmail.com>
2025-08-17 17:50:57 -0600

    Added more integration tests

---
+++ svql_driver/tests/and_tests.rs
@@ -0,0 +1,52 @@
+
+mod integration_tests {
+    #[cfg(test)]
+    mod dff {
+        use std::path::{Path, PathBuf};
+
+        use rstest::rstest;
+        use svql_driver::Driver;
+        use svql_driver::util::load_driver_from;
+
+        lazy_static::lazy_static! {
+
+            static ref AND_Q_DOUBLE_SDFFE: (Driver, PathBuf) = load_driver_from("examples/patterns/basic/ff/and_q_double_sdffe.v");
+            static ref AND_GATE: (Driver, PathBuf) = load_driver_from("examples/patterns/basic/and/and_gate.v");
+            static ref AND_TREE: (Driver, PathBuf) = load_driver_from("examples/patterns/basic/and/and_tree.v");
+            static ref AND_SEQ: (Driver, PathBuf) = load_driver_from("examples/patterns/basic/and/and_seq.v");
+        }
+
+    
+        #[rstest]
+        // AND_Q_DOUBLE_SDFFE Needle
+        #[case(&AND_Q_DOUBLE_SDFFE, &AND_Q_DOUBLE_SDFFE, 2)]
+        #[case(&AND_Q_DOUBLE_SDFFE, &AND_GATE, 0)]
+        #[case(&AND_Q_DOUBLE_SDFFE, &AND_TREE, 0)]
+        #[case(&AND_Q_DOUBLE_SDFFE, &AND_SEQ, 0)]
+        // AND_GATE Needle
+        #[case(&AND_GATE, &AND_Q_DOUBLE_SDFFE, 1)]
+        #[case(&AND_GATE, &AND_GATE, 1)]
+        #[case(&AND_GATE, &AND_TREE, 7)]
+        #[case(&AND_GATE, &AND_SEQ, 7)]
+        // AND_TREE Needle
+        #[case(&AND_TREE, &AND_Q_DOUBLE_SDFFE, 0)]
+        #[case(&AND_TREE, &AND_GATE, 0)]
+        #[case(&AND_TREE, &AND_TREE, 1)]
+        #[case(&AND_TREE, &AND_SEQ, 0)]
+        // AND_SEQ Needle
+        #[case(&AND_SEQ, &AND_Q_DOUBLE_SDFFE, 0)]
+        #[case(&AND_SEQ, &AND_GATE, 0)]
+        #[case(&AND_SEQ, &AND_TREE, 0)]
+        #[case(&AND_SEQ, &AND_SEQ, 1)]
+        fn test_subgraph_matches(
+            #[case] needle_tuple: &'static (Driver, PathBuf),
+            #[case] haystack_tuple: &'static (Driver, PathBuf),
+            #[case] expected: usize,
+        ) {
+            let (needle, needle_path) = needle_tuple;
+            let (haystack, haystack_path) = haystack_tuple;
+            let matches = svql_driver::subgraph::find_subgraphs(needle.design_as_ref(), haystack.design_as_ref());
+            assert_eq!(matches.len(), expected, "Expected {} matches for needle {}, against haystack {}, got {}", expected, needle_path.display(), haystack_path.display(), matches.len());
+        }
+    }
+}
