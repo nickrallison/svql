00b06727d5186c83f63740fc320c2e13d214f9e2
nick <nickrallison1@gmail.com>
2025-08-19 11:33:16 -0600

    added mixed_and_tree

--- scripts/out.sh
+++ scripts/out.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env bash
 
 # md_tree.py --root . \
-#     --include svql_*/**.rs \
-#     --include examples/**.v \
+#     --include 'svql_*/**.rs' \
+#     --include 'examples/**.v' \
 #     --include 'Cargo.toml' \
+#     --include 'README.md' \
 #     --include 'svql_*/Cargo.toml' \
 #     --header-base-level 2 \
 #     --section "examples=Examples" \
 #     --section "svql_subgraph=svql_subgraph" \
 #     --section "svql_driver=svql_driver" \
-#     --section "svql_query=svql_query" > out.md
+#     --section "svql_query=svql_query" \
+#     > out.md
 
 md_tree.py --root . \
     --include 'svql_*/**.rs' \
@@ -21,4 +23,5 @@ md_tree.py --root . \
     --section "examples=Examples" \
     --section "svql_subgraph=svql_subgraph" \
     --section "svql_driver=svql_driver" \
-    --section "svql_query=svql_query" > out.md
+    --section "svql_query=svql_query" \
+    > out.md
