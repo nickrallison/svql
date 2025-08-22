14c20ea2d854333f8ba98815a3b1ee7e6ce0d662
nick <nickrallison1@gmail.com>
2025-08-22 14:33:14 -0600

    updated script

--- scripts/out.sh
+++ scripts/out.sh
@@ -13,15 +13,20 @@
 #     --section "svql_query=svql_query" \
 #     > out.md
 
+mkdir -p generated
+cargo expand --package svql_query queries::netlist > generated/expanded_netlist.rs
+
 python3 scripts/md_tree.py --root . \
     --include 'svql_*/**.rs' \
     --include 'examples/**.v' \
+    --include 'generated/**.rs' \
     --include 'Cargo.toml' \
     --include 'README.md' \
     --include 'svql_*/Cargo.toml' \
     --include 'svql_*/README.md' \
     --header-base-level 2 \
     --section "examples=Examples" \
+    --section "generated=Generated" \
     --section "svql_subgraph=svql_subgraph" \
     --section "svql_driver=svql_driver" \
     --section "svql_query=svql_query" \
