37ed7d1b698be1328eec37644355c3c8751fe752
nick <nickrallison1@gmail.com>
2025-08-26 10:18:18 -0600

    refactoring tests

--- svql_common/src/lib.rs
+++ svql_common/src/lib.rs
@@ -1,7 +1,9 @@
+pub mod build_support;
 mod config;
 mod test_cases;
 mod yosys;
 
+// pub use crate::build_support::*;
 pub use crate::config::*;
 pub use crate::test_cases::*;
 pub use crate::yosys::*;
