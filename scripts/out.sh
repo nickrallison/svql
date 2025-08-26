7f02294f07a44843f90226a206b6c2da36378e65
nick <nickrallison1@gmail.com>
2025-08-25 20:18:42 -0600

    driver uses build.rs

--- scripts/out.sh
+++ scripts/out.sh
@@ -1,36 +1,16 @@
 #! /usr/bin/env bash
 
 mkdir -p generated
-# cargo expand --package svql_query queries::netlist > generated/expanded_netlist.rs
-
-# python3 scripts/md_tree.py --root . \
-#     --include 'svql_*/**.rs' \
-#     --include 'examples/**.v' \
-#     --include 'generated/**.rs' \
-#     --include 'Cargo.toml' \
-#     --include 'README.md' \
-#     --include 'svql_*/Cargo.toml' \
-#     --include 'svql_*/README.md' \
-#     --header-base-level 2 \
-#     --section "examples=Examples" \
-#     --section "generated=Generated" \
-#     --section "svql_subgraph=svql_subgraph" \
-#     --section "svql_driver=svql_driver" \
-#     --section "svql_query=svql_query" \
-#     > out.md
-
-# mkdir -p generated
-# cargo expand --package svql_query queries::netlist > generated/expanded_netlist.rs
+cargo expand --package svql_query queries::netlist > generated/expanded_netlist.rs
 
 python3 scripts/md_tree.py --root . \
-    --include 'prjunnamed/netlist/src/design.rs' \
-    --include 'prjunnamed/netlist/src/cell.rs' \
-    --include 'svql_subgraph/**.rs' \
+    --include 'svql_*/**.rs' \
     --include 'examples/**.v' \
+    --include 'generated/**.rs' \
     --include 'Cargo.toml' \
     --include 'README.md' \
-    --include 'svql_subgraph/Cargo.toml' \
-    --include 'svql_subgraph/README.md' \
+    --include 'svql_*/Cargo.toml' \
+    --include 'svql_*/README.md' \
     --header-base-level 2 \
     --section "examples=Examples" \
     --section "generated=Generated" \
@@ -38,3 +18,23 @@ python3 scripts/md_tree.py --root . \
     --section "svql_driver=svql_driver" \
     --section "svql_query=svql_query" \
     > out.md
+
+# mkdir -p generated
+# cargo expand --package svql_query queries::netlist > generated/expanded_netlist.rs
+
+# python3 scripts/md_tree.py --root . \
+#     --include 'prjunnamed/netlist/src/design.rs' \
+#     --include 'prjunnamed/netlist/src/cell.rs' \
+#     --include 'svql_subgraph/**.rs' \
+#     --include 'examples/**.v' \
+#     --include 'Cargo.toml' \
+#     --include 'README.md' \
+#     --include 'svql_subgraph/Cargo.toml' \
+#     --include 'svql_subgraph/README.md' \
+#     --header-base-level 2 \
+#     --section "examples=Examples" \
+#     --section "generated=Generated" \
+#     --section "svql_subgraph=svql_subgraph" \
+#     --section "svql_driver=svql_driver" \
+#     --section "svql_query=svql_query" \
+#     > out.md
