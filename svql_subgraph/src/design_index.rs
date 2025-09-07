d543cbc2a2ee3da12fd6c3f76e4699658553607e
nick <nickrallison1@gmail.com>
2025-09-07 17:46:11 -0600

    added input fanin map

--- svql_subgraph/src/design_index.rs
+++ svql_subgraph/src/design_index.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 
-use prjunnamed_netlist::{CellRef, Design};
+use prjunnamed_netlist::{Cell, CellRef, Design};
 
 use crate::{
     Timer,
@@ -18,6 +18,9 @@ pub struct DesignIndex<'a> {
     // ##########################################
     input_by_name: HashMap<&'a str, CellWrapper<'a>>,
     output_by_name: HashMap<&'a str, CellWrapper<'a>>,
+
+    // Input Fanout map
+    input_fanin_map: HashMap<String, Vec<(CellWrapper<'a>, usize)>>,
 }
 
 impl<'a> DesignIndex<'a> {
@@ -41,6 +44,10 @@ impl<'a> DesignIndex<'a> {
         let input_by_name = Self::build_input_by_name(&cells_topo);
         let output_by_name = Self::build_output_by_name(&cells_topo);
 
+        // #############
+        let input_fanin_map: HashMap<String, Vec<(CellWrapper<'a>, usize)>> =
+            Self::build_input_fanin_map(&cells_topo, design);
+
         DesignIndex {
             cells_topo,
             cell_type_map,
@@ -48,6 +55,8 @@ impl<'a> DesignIndex<'a> {
             //
             input_by_name,
             output_by_name,
+            //
+            input_fanin_map,
         }
     }
 
@@ -139,6 +148,23 @@ impl<'a> DesignIndex<'a> {
         output_by_name
     }
 
+    fn build_input_fanin_map(
+        cells_topo: &[CellWrapper<'a>],
+        design: &'a Design,
+    ) -> HashMap<String, Vec<(CellWrapper<'a>, usize)>> {
+        let mut input_fanin_map: HashMap<String, Vec<(CellWrapper<'a>, usize)>> = HashMap::new();
+        for cell in cells_topo {
+            let input_fanin = get_cell_index_fanin(cell.get(), design);
+            for (input_name, input_idx) in input_fanin {
+                input_fanin_map
+                    .entry(input_name)
+                    .or_default()
+                    .push((cell.clone(), input_idx));
+            }
+        }
+        input_fanin_map
+    }
+
     pub fn get_by_type(&self, node_type: CellType) -> &[CellWrapper<'a>] {
         let _t = Timer::new("GraphIndex::get_by_type");
         self.cell_type_map
@@ -157,5 +183,205 @@ impl<'a> DesignIndex<'a> {
 
     pub fn get_output_by_name(&self) -> &HashMap<&'a str, CellWrapper<'a>> {
         &self.output_by_name
+    }
+}
+
+pub fn get_cell_index_fanin<'a>(
+    cell: &Cell,
+    design: &prjunnamed_netlist::Design,
+) -> Vec<(String, usize)> {
+    use prjunnamed_netlist::Cell::*;
+    match cell {
+        Buf(p_value) => get_value_index_fanin(p_value, design),
+        Not(p_value) => get_value_index_fanin(p_value, design),
+        And(p_a_value, p_b_value) => {
+            let mut a_matches = get_value_index_fanin(p_a_value, design);
+            let b_matches = get_value_index_fanin(p_b_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        Or(p_a_value, p_b_value) => {
+            let mut a_matches = get_value_index_fanin(p_a_value, design);
+            let b_matches = get_value_index_fanin(p_b_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        Xor(p_a_value, p_b_value) => {
+            let mut a_matches = get_value_index_fanin(p_a_value, design);
+            let b_matches = get_value_index_fanin(p_b_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        Mux(p_a_net, p_b_value, p_c_value) => {
+            let mut a_matches = get_net_index_fanin(p_a_net, design);
+            let b_matches = get_value_index_fanin(p_b_value, design);
+            let c_matches = get_value_index_fanin(p_c_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches.extend(c_matches.into_iter());
+            a_matches
+        }
+        Adc(p_a_value, p_b_value, p_ci_net) => {
+            let mut a_matches = get_value_index_fanin(p_a_value, design);
+            let b_matches = get_value_index_fanin(p_b_value, design);
+            let ci_matches = get_net_index_fanin(p_ci_net, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches.extend(ci_matches.into_iter());
+            a_matches
+        }
+        Aig(pa_control_net, pb_control_net) => {
+            let mut a_matches = get_control_net_index_fanin(pa_control_net, design);
+            let b_matches = get_control_net_index_fanin(pb_control_net, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        Eq(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        ULt(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        SLt(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        Shl(pa_value, pb_value, _pc_u32) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        UShr(pa_value, pb_value, _pc_u32) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        SShr(pa_value, pb_value, _pc_u32) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        XShr(pa_value, pb_value, _pc_u32) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        Mul(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        UDiv(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        UMod(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        SDivTrunc(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        SDivFloor(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        SModTrunc(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        SModFloor(pa_value, pb_value) => {
+            let mut a_matches = get_value_index_fanin(pa_value, design);
+            let b_matches = get_value_index_fanin(pb_value, design);
+            a_matches.extend(b_matches.into_iter());
+            a_matches
+        }
+        Match(_p_match_cell) => {
+            todo!("Make Function to match match cells")
+        }
+        Assign(_p_assign_cell) => {
+            todo!("Make Function to match assign cells")
+        }
+        Dff(p_dff_cell) => {
+            let mut data_matches = get_value_index_fanin(&p_dff_cell.data, design);
+            let clock_matches = get_control_net_index_fanin(&p_dff_cell.clock, design);
+            let clear_matches = get_control_net_index_fanin(&p_dff_cell.clear, design);
+            let reset_matches = get_control_net_index_fanin(&p_dff_cell.reset, design);
+            let enable_matches = get_control_net_index_fanin(&p_dff_cell.enable, design);
+            data_matches.extend(clock_matches.into_iter());
+            data_matches.extend(clear_matches.into_iter());
+            data_matches.extend(reset_matches.into_iter());
+            data_matches.extend(enable_matches.into_iter());
+            data_matches
+        }
+        Memory(_p_memory_cell) => {
+            todo!("Make Function to match memory cells")
+        }
+        // (IoBuf(pi), IoBuf(di)) => todo!(),
+        // (Target(pt), Target(dt)) => todo!(),
+        // (Other(po), Other(do_)) => todo!(),
+        Input(_p_name, _p_width) => return vec![],
+        // (Output(pn, pv), Output(dn, dv)) => todo!(),
+        // (Name(pn, pv), Name(dn, dv)) => todo!(),
+        // (Debug(pn, pv), Debug(dn, dv)) => todo!(),
+        _ => vec![],
+    }
+}
+
+fn get_value_index_fanin<'a>(
+    value: &prjunnamed_netlist::Value,
+    design: &prjunnamed_netlist::Design,
+) -> Vec<(String, usize)> {
+    match &value.0 {
+        prjunnamed_netlist::ValueRepr::None => vec![],
+        prjunnamed_netlist::ValueRepr::Some(net) => get_net_index_fanin(&net, design),
+        prjunnamed_netlist::ValueRepr::Many(nets) => nets
+            .iter()
+            .flat_map(|net| get_net_index_fanin(&net, design))
+            .collect(),
+    }
+}
+fn get_net_index_fanin<'a>(
+    net: &prjunnamed_netlist::Net,
+    design: &prjunnamed_netlist::Design,
+) -> Vec<(String, usize)> {
+    if let Ok((cell_ref, id)) = design.find_cell(*net) {
+        let cell_wrapper: CellWrapper = cell_ref.into();
+        if let Some(name) = cell_wrapper.input_name() {
+            return vec![(name.to_string(), id)];
+        }
+    }
+    vec![]
+}
+
+fn get_control_net_index_fanin<'a>(
+    pa_control_net: &prjunnamed_netlist::ControlNet,
+    design: &prjunnamed_netlist::Design,
+) -> Vec<(String, usize)> {
+    match pa_control_net {
+        prjunnamed_netlist::ControlNet::Pos(net) => get_net_index_fanin(net, design),
+        prjunnamed_netlist::ControlNet::Neg(net) => get_net_index_fanin(net, design),
     }
 }
