55a20e2ebd0b357f8ece5ab9cf69c4b4d69ef296
nick <nickrallison1@gmail.com>
2025-10-02 16:26:43 -0600

    scripts for flatten

--- scripts/cores_download/hackatdac21/openpiton_tile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# yosys -p "verific -sv build/openpiton__chip_0.1/pickle-icarus/openpiton__chip_0.1.v" -p "hierarchy -top chip" -p proc -p memory -p opt_clean -p flatten -p "write_json examples/fixtures/larger_designs/json/openpiton_chip_full.json"
-# yosys -p "verific -sv examples/fixtures/larger_designs/json/openpiton_tile.v" -p "hierarchy -top tile" -p proc -p memory -p opt_clean -p flatten -p "write_json examples/fixtures/larger_designs/json/openpiton_tile_full.json"
-# yosys -p "verific -sv examples/fixtures/larger_designs/json/openpiton_tile.v" -p "hierarchy -top tile" -p proc -p memory -p opt_clean -p flatten -p "write_json examples/fixtures/larger_designs/json/openpiton_tile_full.json"
-yosys -p "verific -sv examples/fixtures/larger_designs/verilog/openpiton_tile.v" -p "hierarchy -top tile" -p proc -p memory -p opt_clean -p "write_json examples/fixtures/larger_designs/json/openpiton_tile_partial.json"
-gzip examples/fixtures/larger_designs/json/openpiton_tile_partial.json
