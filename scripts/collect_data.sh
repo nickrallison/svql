921b1e44917273e67033d6638d523aa80145801d
nick <nickrallison1@gmail.com>
2025-12-27 11:38:51 -0700

    added script to run  add collect data

---
+++ scripts/collect_data.sh
@@ -0,0 +1,7 @@
+export RUST_LOG=info 
+
+/bin/time cargo run --bin example_query --release --features svql_subgraph/rayon --features svql_query/parallel -- examples/fixtures/larger_designs/json/hackatdac18/cv32e40p_fp_wrapper_netlist.json cv32e40p_fp_wrapper 3
+/bin/time cargo run --bin example_query --release --features svql_subgraph/rayon --features svql_query/parallel -- examples/fixtures/larger_designs/json/hackatdac18/soc_interconnect_wrap_netlist.json soc_interconnect_wrap 2
+/bin/time cargo run --bin example_query --release --features svql_subgraph/rayon --features svql_query/parallel -- examples/fixtures/larger_designs/json/hackatdac18/soc_peripherals_netlist.json soc_peripherals 3
+/bin/time cargo run --bin example_query --release --features svql_subgraph/rayon --features svql_query/parallel -- examples/fixtures/larger_designs/json/hackatdac18/udma_core_netlist.json udma_core 3
+
