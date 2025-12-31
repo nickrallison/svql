5ed92813c2bea21150925891d2f942ea1aa1e58d
nick <nickrallison1@gmail.com>
2025-12-31 16:05:08 -0700

    fixing up locked reg

--- svql_query/src/security/primitives/locked_register.rs
+++ svql_query/src/security/primitives/locked_register.rs
@@ -1,29 +1,7 @@
-use crate::{State, Wire};
+use crate::State;
+use crate::Wire;
+use crate::primitives::dff::{Adffe, Sdffe};
 use svql_macros::{netlist, variant};
-
-#[netlist(
-    file = "examples/patterns/security/access_control/locked_reg/rtlil/async_en.il",
-    name = "async_en"
-)]
-pub struct AsyncDffEnable<S: State> {
-    pub clk: Wire<S>,
-    pub data_in: Wire<S>,
-    pub resetn: Wire<S>,
-    pub write_en: Wire<S>,
-    pub data_out: Wire<S>,
-}
-
-#[netlist(
-    file = "examples/patterns/security/access_control/locked_reg/rtlil/sync_en.il",
-    name = "sync_en"
-)]
-pub struct SyncDffEnable<S: State> {
-    pub clk: Wire<S>,
-    pub data_in: Wire<S>,
-    pub resetn: Wire<S>,
-    pub write_en: Wire<S>,
-    pub data_out: Wire<S>,
-}
 
 #[netlist(
     file = "examples/patterns/security/access_control/locked_reg/rtlil/sync_mux.il",
@@ -53,21 +31,21 @@ pub struct AsyncDffMuxEnable<S: State> {
 pub enum LockedRegister<S: State> {
     #[variant(map(
         clk = "clk",
-        data_in = "data_in",
-        data_out = "data_out",
-        resetn = "resetn",
-        write_en = "write_en"
+        data_in = "d",
+        data_out = "q",
+        resetn = "reset_n",
+        write_en = "en"
     ))]
-    AsyncEn(AsyncDffEnable<S>),
+    AsyncEn(Adffe<S>),
 
     #[variant(map(
         clk = "clk",
-        data_in = "data_in",
-        data_out = "data_out",
-        resetn = "resetn",
-        write_en = "write_en"
+        data_in = "d",
+        data_out = "q",
+        resetn = "reset",
+        write_en = "en"
     ))]
-    SyncEn(SyncDffEnable<S>),
+    SyncEn(Sdffe<S>),
 
     #[variant(map(
         clk = "clk",
@@ -95,8 +73,8 @@ impl<S: State> LockedRegister<S> {
 
     pub fn register_type(&self) -> String {
         match self {
-            LockedRegister::AsyncEn(_) => "AsyncDffEnable".to_string(),
-            LockedRegister::SyncEn(_) => "SyncDffEnable".to_string(),
+            LockedRegister::AsyncEn(_) => "Adffe (Primitive)".to_string(),
+            LockedRegister::SyncEn(_) => "Sdffe (Primitive)".to_string(),
             LockedRegister::AsyncMux(_) => "AsyncDffMuxEnable".to_string(),
             LockedRegister::SyncMux(_) => "SyncDffMuxEnable".to_string(),
             _ => "Unknown".to_string(),
