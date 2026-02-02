f7bb24d71fbaa28c2217633e23fb149ff7eb35f7
nickrallison <nickrallison1@gmail.com>
2026-02-02 16:26:30 -0700

    fixing test path

--- svql_subgraph/tests/locked_reg.rs
+++ svql_subgraph/tests/locked_reg.rs
@@ -9,7 +9,7 @@ fn cwe1234_config(c: ConfigBuilder) -> ConfigBuilder {
 query_test!(
     name: test_adffe_mux_enable_multi_width,
     needle: ("examples/patterns/security/access_control/locked_reg/rtlil/async_mux.il", "async_mux"),
-    haystack: ("examples/fixtures/cwes/cwe1234/cwe1234_multi_width.il", "cwe1234_multi_width"),
+    haystack: ("examples/fixtures/cwes/cwe1234/cwe1234_multi_width.v", "cwe1234_multi_width"),
     expect: 5,
     config: cwe1234_config
 );
